29b0b1debaac7dcd844977f73c0f8a115013cc48
thunder_burn <ml@viralwunder.de>
2024-06-15 18:03:42 +0200

    added github environment variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-# starship init fish | source
+#starship init fish | source
+set -x GCM_CREDENTIAL_STORE secretservice
 fish_config theme choose Lava
 oh-my-posh init fish --config ~/.config/oh-my-posh/config.toml | source
 zoxide init fish | source
