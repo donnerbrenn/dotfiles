a8e6003873952a8ab13fc7aef191557a7a3dd930
thunder_burn <ml@viralwunder.de>
2024-06-18 14:51:30 +0200

    beautify lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,6 +8,14 @@ return {
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
 			},
+			sections = {
+				lualine_a = { { "mode", icon = "" } },
+				lualine_b = { { "filename", icon = "" } },
+				lualine_c = { { "branch", icon = "" } },
+				lualine_x = { { "encoding", "system", icon = "󰅨" } },
+				lualine_y = { { "filetype" } },
+				lualine_z = { { "location", icon = "" } },
+			},
 		})
 	end,
 }
