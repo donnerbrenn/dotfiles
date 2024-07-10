daf8bee1558267f1180adceb3014deec71a5ca6a
Manuel Lehmann <ml@viralwunder.de>
2024-07-10 13:06:43 +0200

    clean up

--- .config/nvim/lua/plugins/treesj.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"Wansmer/treesj",
-	keys = { "<leader>11", "<space>j", "<space>s" },
-	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
-	config = function()
-		require("treesj").setup({})
-	end,
-}
