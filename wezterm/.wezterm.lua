2e03cbdeedfdf861a6ee764153f2efd8cdd1c628
Manuel Lehmann <ml@viralwunder.de>
2024-09-07 16:07:47 +0200

    new colors for wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,19 +10,37 @@ end)
 -- This will hold the configuration
 local config = wezterm.config_builder()
 -- Window
-config.max_fps = 144
-config.window_background_opacity = 0.95
+config.max_fps = 60
+config.window_background_opacity = 0.9
 -- Style
 config.enable_tab_bar = false
-config.color_scheme = "ChallengerDeep"
+config.color_scheme = "AdventureTime"
+config.colors = {
+	cursor_bg = "white",
+	foreground = "white",
+	selection_fg = "white",
+	selection_bg = "green",
+	brights = {
+		"grey",
+		"red",
+		"lime",
+		"yellow",
+		"#82aaff", --folders
+		"fuchsia",
+		"#82df4a", --executables
+		"white",
+	},
+}
 config.window_close_confirmation = "NeverPrompt"
 -- Font
 config.font = wezterm.font("RobotoMonoNerdFont")
-config.font_size = 16
+config.font_size = 17
 -- Cursor
+--
 config.default_cursor_style = "BlinkingBlock"
-config.animation_fps = 144
+config.animation_fps = 60
 config.cursor_blink_rate = 500
 config.cursor_blink_ease_in = "EaseOut"
 config.cursor_blink_ease_out = "EaseOut"
+config.hide_mouse_cursor_when_typing = true
 return config
