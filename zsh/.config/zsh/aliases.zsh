0165a99cf15cd7e111b28e32fc94128ff4a049b5
Manuel Lehmann <ml@viralwunder.de>
2025-06-22 10:36:03 +0200

    Maded aliases a little bit nicer

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,41 +1,32 @@
+# alias aliases='alias | cut -d '=' -f 1 | fzf' #search aliases
+alias v='nvim' #start nvim
+alias w='curl wttr.in' #show weather
+alias ffxiv='cd ~/Developement/FFXIV && ./create.py' #fetch ffxiv stats
+alias asap='cd ~/Developement/asap && ./create.py' #fetch asap stats
+alias y='yay' #call yay
+alias off='systemctl poweroff' #turn off computer
+alias reboot='systemctl reboot' #reboot computer
+alias l='exa --icons -l' #shortcut for exa
+alias ls='exa --icons' #replace ls by exa
+alias la='exa --icons -la' #long exa
+alias lh='exa --icons -lah' #long exa with hidden files
+alias ff='fzf --preview="bat --color=always {}"' #find a file or directory
+alias fff='find . -type f | fzf --preview "bat --color=always {}"' #find a file
+alias b='time cat ~/bible.txt' #benchmark terminal with the full bible
+alias man='batman' #replace man by batman
+alias tteasy='tt -words ~/tt/wortliste-easy.txt' #start a typing test with easy words
+alias tthard='tt -words ~/tt/wortliste-hard.txt' #start a typing test with hard words
+alias ttrare='tt -words ~/tt/wortliste-rare.txt' #start a typing test with rare letter words
+alias ttrt='tt -words ~/tt/wortliste-rtui.txt' #start a typing test with r/t/u/i letter words
+alias please='sudo $(fc -ln -1)' #rerun the last command with sudo
+alias ccleaner='sudo paccache -r && yay -Sc --noconfirm' #purge pacman and yay caches
+alias pacc='sudo pacman -Rns $(pacman -Qdtq)' #remove orphaned packages
+alias sysf='systemctl list-units --type=service --all | fzf | awk '{print $1}' | xargs -r systemctl restart' #find an (re)start services
+alias jcf=' journalctl --no-pager -o short-iso | fzf --ansi --reverse' #search through the system journal
+alias yaya="yay -Sl | awk '\$1 == \"aur\" { print \$2 \"\t\" \$1 }' | sort | fzf --multi --with-nth=1 --preview 'yay -Si {1}' --preview-window=right:85% | awk '{print \$1}' | xargs -ro yay -S" #install packages from AUR
 
-alias v='nvim'
-alias w='curl wttr.in'
-alias ffxiv='cd ~/Developement/FFXIV && ./create.py'
-alias asap='cd ~/Developement/asap && ./create.py'
-alias y='yay'
-alias off='systemctl poweroff'
-alias reboot='systemctl reboot'
-alias l='exa --icons -l'
-alias ls='exa --icons'
-alias la='exa --icons -la'
-alias lh='exa --icons -lah'
-alias ff='fzf --preview="bat --color=always {}"'
-alias fff='find . -type f | fzf --preview "bat --color=always {}"'
-alias b='time cat ~/bible.txt'
-alias man='batman'
-alias tteasy='tt -words ~/tt/wortliste-easy.txt'
-alias tthard='tt -words ~/tt/wortliste-hard.txt'
-alias ttrare='tt -words ~/tt/wortliste-rare.txt'
-alias ttrt='tt -words ~/tt/wortliste-rtui.txt'
-alias please='sudo $(fc -ln -1)'
-alias ccleaner='sudo paccache -r && yay -Sc --noconfirm'
-alias pacc='sudo pacman -Rns $(pacman -Qdtq)'
-alias sysf='systemctl list-units --type=service --all | fzf | awk '{print $1}' | xargs -r systemctl restart'
-alias jcf=' journalctl --no-pager -o short-iso | fzf --ansi --reverse'
-alias yaya="yay -Sl | awk '\$1 == \"aur\" { print \$2 \"\t\" \$1 }' | sort | fzf --multi --with-nth=1 --preview 'yay -Si {1}' --preview-window=right:85% | awk '{print \$1}' | xargs -ro yay -S"
+alias yayf="yay -Sl | awk ' \$1 == \"cachyos-znver4\" { seen[\$2] = \$1 } !(\$2 in seen) { seen[\$2] = \$1 } END { for (pkg in seen) printf \"%s\t%s\n\", pkg, seen[pkg] } ' | sort | fzf --multi --with-nth=1 --preview 'yay -Si {1}' --preview-window=right:75% | awk '{print \$1}' | xargs -ro yay -S" #search packages via yay
 
-alias yayf="yay -Sl | awk '
-  \$1 == \"cachyos-znver4\" { seen[\$2] = \$1 }
-  !(\$2 in seen) { seen[\$2] = \$1 }
-  END { for (pkg in seen) printf \"%s\t%s\n\", pkg, seen[pkg] }
-' | sort | fzf --multi --with-nth=1 --preview 'yay -Si {1}' --preview-window=right:75% | awk '{print \$1}' | xargs -ro yay -S"
-# alias pacf="pacman -Sl | fzf --multi --preview 'pacman -Sii {2}' --preview-window=right:50% | awk '{print \$2}' | xargs -ro sudo pacman -S"
+alias pacf="pacman -Sl | awk ' \$1 == \"cachyos-znver4\" { seen[\$2] = \$1 } !(\$2 in seen) { seen[\$2] = \$1 } END { for (pkg in seen) printf \"%s\t%s\n\", pkg, seen[pkg] } ' | sort | fzf --multi --with-nth=1 --preview 'pacman -Sii {1}' --preview-window=right:75% | awk '{print \$1}' | xargs -ro sudo pacman -S" #search packages via pacman
 
-alias pacf="pacman -Sl | awk '
-  \$1 == \"cachyos-znver4\" { seen[\$2] = \$1 }
-  !(\$2 in seen) { seen[\$2] = \$1 }
-  END { for (pkg in seen) printf \"%s\t%s\n\", pkg, seen[pkg] }
-' | sort | fzf --multi --with-nth=1 --preview 'pacman -Sii {1}' --preview-window=right:75% | awk '{print \$1}' | xargs -ro sudo pacman -S"
-
-alias pacli='pacman -Qq | fzf --preview "echo \"--- Paketinfo ---\" && pacman -Qi {} && echo && echo \"--- Dateien ---\" && pacman -Ql {} | head -40" --preview-window=right:85%'
+alias pacli='pacman -Qq | fzf --preview "echo \"--- Paketinfo ---\" && pacman -Qi {} && echo && echo \"--- Dateien ---\" && pacman -Ql {} | head -40" --preview-window=right:85%' #search through installed packages
--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -7,5 +7,29 @@ reload_zshrc() {
   zle reset-prompt
 }
 
+aliases() {
+  local file=~/.config/zsh/aliases.zsh 
+  local selected_line alias_name
+
+    selected_line=$(awk -F'#' '
+      /^alias / {
+        split($1, a, " ")
+        split(a[2], b, "=")
+        name = b[1]
+        desc = $2
+        # Tab oder spaces zum besseren Layout
+        printf "%-20s # %s\n", name, desc
+      }
+    ' "$file" | fzf --ansi --preview "grep -E '^alias {1}' $file")
+
+    # Nur den Alias-Namen extrahieren (vor dem #)
+    alias_name=$(echo "$selected_line" | awk '{print $1}')
+    
+    if [[ -n $alias_name ]]; then
+      # eval alias ausführen
+      eval "$alias_name"
+    fi
+}
 
 zle -N reload_zshrc
+zle -N aliases
