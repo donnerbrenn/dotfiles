85798db63a16f3a46ced8f50df6a80df9f7b2137
Manuel Lehmann <ml@viralwunder.de>
2025-06-25 22:05:55 +0200

    configuring zinit

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -2,7 +2,10 @@
 if [[ ! -f "${HOME}/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
     mkdir -p "${HOME}/.local/share/zinit"
     git clone https://github.com/zdharma-continuum/zinit.git "${HOME}/.local/share/zinit/zinit.git"
+
 fi
+export ZSH_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/zinit"
+export ZPFX="${HOME}/.local/share/zinit/polaris"
 
 # Zinit laden
 source "${HOME}/.local/share/zinit/zinit.git/zinit.zsh"
@@ -13,13 +16,3 @@ compinit
 
 zinit load Aloxaf/fzf-tab
 zinit load zsh-users/zsh-syntax-highlighting
-
-
-#
-#
-# plugins=(
-#     git                         # Standard‑Git‑Aliaspaket
-#     fzf                         # fuzzy finder in $PATH
-#     zsh-autosuggestions         # Tipp‑Vervollständigung aus History
-#     zsh-syntax-highlighting     # farbige Syntax während des Tippens
-# )
